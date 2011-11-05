c9499495e1945d28096cb74e8065ff507dc8744c
Martin Felke <martin.felke@googlemail.com>
2011-11-05 11:29:12 +0100

    added part of control logic for the destruction itself

--- destruction_proc.py
+++ destruction_proc.py
@@ -6,124 +6,140 @@ from . import destruction_data as dd
 
 class Processor():
     
-    modes = { }
-    
-    def processDestruction(context, mode, parts, granularity, thickness):
+   
+                 
+    def processDestruction(self, context):
         self.context = context
+        
+        modes = {DestructionContext.destModes[0][0]: "self.applyFracture",
+                 DestructionContext.destModes[1][0]: "self.applyExplo",
+                 DestructionContext.destModes[2][0]: "self.previewExplo" } 
         #make an object backup if necessary (if undo doesnt handle this)
         #according to mode call correct method
-        #if (parts > 1):
+        mode = context.object.destruction.destructionMode
+        parts = context.object.destruction.partCount
+        granularity = context.object.destruction.pieceGranularity
+        thickness = context.object.destruction.wallThickness
+        args = [parts, granularity, thickness]
+        
+        if (parts > 1):
+            print(mode, modes[mode])
+            eval(modes[mode])
         
         return None
     
     
-    def previewExplo(parts, granularity, thickness):
+    def previewExplo(self, parts, granularity, thickness):
         #create modifiers if not there
+        print("previewExplo", parts, granularity, thickness)
         
-        #set data to modifiers
-        pass
+        return "previewExplo2"
+        
     
-    def applyExplo():
+    def applyExplo(self, parts, granularity, thickness):
         #create objects from explo by applying it(or by loose parts)
         #check modifier sequence before applying it 
         #(if all are there; for now no other modifiers allowed in between)
+        print("applyExplo", parts, granularity, thickness)
         
         #and parent them all to an empty
-        pass  
+          
     
-    def applyFracture(parts):
+    def applyFracture(self,parts):
         #make fracture gui available as sublayout in panel
         #simply call the operator and parent the context
         #children with same Name and different number
         #when applying hierarchical fracturing, append hierarchy level number to part number
         #_1, _2 and so on
-        pass  
+        print("applyFracture", parts)  
 
 def updateGrid(self, context):
-	obj = context.object
-	dim = obj.bound_box.data.dimensions.to_tuple()
-	dd.DataStore.grid = dd.Grid(self.grid, obj.location.to_tuple(), dim, obj.children)
-	dd.DataStore.grid.buildNeighborhood()
-	print(dd.DataStore.grid)
-	return None
+    obj = context.object
+    dim = obj.bound_box.data.dimensions.to_tuple()
+    dd.DataStore.grid = dd.Grid(self.grid, obj.location.to_tuple(), dim, obj.children)
+    dd.DataStore.grid.buildNeighborhood()
+    print(dd.DataStore.grid)
+    return None
 
 def updateDestructionMode(self, context):
-	return None
+    return None
 
 def updatePartCount(self, context):
-	return None
+    dd.DataStore.proc.processDestruction(context)
+    return None
 
 def updateWallThickness(self, context):
-	return None
+    return None
 
 def updatePieceGranularity(self, context):
-	return None
+    return None
 
 def updateIsGround(self, context):
-	return None
+    return None
 
 
 def updateGroundConnectivity(self, context):
-	return None
+    return None
 
 #def updateGrounds(self, context)
 #   pass
 #template_list -> Operators
 
 def updateDestructor(self, context):
-	return None
+    return None
 
 #def updateTargets(self, context)
 #   pass
 #template_list -> Operators
 
 def updateTransmitMode(self, context):
-	return None    
+    return None 
         
 
 class DestructionContext(types.PropertyGroup):
-	
-	destModes = [('DESTROY_F', 'Apply Fracture Addon', 'Destroy this object using the fracture addon', 0 ), 
-			 ('DESTROY_E', 'Apply Explosion Modifier', 'Destroy this object using the explosion modifier', 1),
-			 ('DESTROY_P', 'Preview Explosion Modifier', 'Preview destruction with explosion modifier', 2)] 
-	
-	transModes = [('T_SELF', 'This Object Only', 'Apply settings to this object only', 0), 
-			 ('T_CHILDREN', 'Direct Children', 'Apply settings to direct children as well', 1),
-			 ('T_ALL_CHILDREN', 'All Descendants', 'Apply settings to all descendants as well', 2),
-			 ('T_SELECTED', 'Selected Objects', 'Apply settings to all selected as well', 3),
-			 ('T_LAYERS', 'Active Layers', 'Apply settings to all objects on active layers as well', 4), 
-			 ('T_ALL', 'All Objects', 'Apply settings to all objects as well', 5) ]
-	
+    
+    destModes = [('DESTROY_F', 'Apply Fracture Addon', 'Destroy this object using the fracture addon', 0 ), 
+             ('DESTROY_E', 'Apply Explosion Modifier', 'Destroy this object using the explosion modifier', 1),
+             ('DESTROY_P', 'Preview Explosion Modifier', 'Preview destruction with explosion modifier', 2)] 
+    
+    transModes = [('T_SELF', 'This Object Only', 'Apply settings to this object only', 0), 
+             ('T_CHILDREN', 'Direct Children', 'Apply settings to direct children as well', 1),
+             ('T_ALL_CHILDREN', 'All Descendants', 'Apply settings to all descendants as well', 2),
+             ('T_SELECTED', 'Selected Objects', 'Apply settings to all selected as well', 3),
+             ('T_LAYERS', 'Active Layers', 'Apply settings to all objects on active layers as well', 4), 
+             ('T_ALL', 'All Objects', 'Apply settings to all objects as well', 5) ]
+    
    # nested = props.FloatProperty(name="Nested", default=0.0)
-	destroyable = props.BoolProperty(name = "destroyable",
-						 description = "This object can be destroyed, according to parent relations")
-	
-	partCount = props.IntProperty(name = "partCount", default = 1, min = 1, max = 999, update = updatePartCount)
-	destructionMode = props.EnumProperty(items = destModes, update = updateDestructionMode)
-	destructor = props.BoolProperty(name = "destructor", 
-						description = "This object can trigger destruction", update = updateDestructor)
-	isGround = props.BoolProperty(name = "isGround", 
-	 description = "This object serves as a hard point, objects not connected to it will be destroyed",
-	 update = updateIsGround)
-	 
-	groundConnectivity = props.BoolProperty(name = "groundConnectivity", 
-	description = "Determines whether connectivity of parts of this object is calculated, so only unconnected parts collapse according to their parent relations", update = updateGroundConnectivity)
-	grid = props.IntVectorProperty(name = "grid", default = (1, 1, 1), min = 1, max = 100, 
-										  subtype ='XYZ', update = updateGrid )
-	destructorTargets = props.CollectionProperty(type = types.PropertyGroup, name = "destructorTargets")
-	grounds = props.CollectionProperty(type = types.PropertyGroup, name = "grounds")
-	transmitMode = props.EnumProperty(items = transModes, name = "Transmit Mode", update = updateTransmitMode)
-	active_target = props.IntProperty(name = "active_target", default = 0)
-	active_ground = props.IntProperty(name = "active_ground", default = 0)
-	groundSelector = props.StringProperty(name = "groundSelector")
-	targetSelector = props.StringProperty(name = "targetSelector")
+    destroyable = props.BoolProperty(name = "destroyable",
+                         description = "This object can be destroyed, according to parent relations")
+    
+    partCount = props.IntProperty(name = "partCount", default = 1, min = 1, max = 999, update = updatePartCount)
+    destructionMode = props.EnumProperty(items = destModes, update = updateDestructionMode)
+    destructor = props.BoolProperty(name = "destructor", 
+                        description = "This object can trigger destruction", update = updateDestructor)
+    isGround = props.BoolProperty(name = "isGround", 
+     description = "This object serves as a hard point, objects not connected to it will be destroyed",
+     update = updateIsGround)
+     
+    groundConnectivity = props.BoolProperty(name = "groundConnectivity", 
+    description = "Determines whether connectivity of parts of this object is calculated, so only unconnected parts collapse according to their parent relations", update = updateGroundConnectivity)
+    grid = props.IntVectorProperty(name = "grid", default = (1, 1, 1), min = 1, max = 100, 
+                                          subtype ='XYZ', update = updateGrid )
+    destructorTargets = props.CollectionProperty(type = types.PropertyGroup, name = "destructorTargets")
+    grounds = props.CollectionProperty(type = types.PropertyGroup, name = "grounds")
+    transmitMode = props.EnumProperty(items = transModes, name = "Transmit Mode", update = updateTransmitMode)
+    active_target = props.IntProperty(name = "active_target", default = 0)
+    active_ground = props.IntProperty(name = "active_ground", default = 0)
+    groundSelector = props.StringProperty(name = "groundSelector")
+    targetSelector = props.StringProperty(name = "targetSelector")
 
-	wallThickness = props.IntProperty(name = "wallThickness", default = 1, min = 1, max = 100,
-									  update = updateWallThickness)
-	pieceGranularity = props.IntProperty(name = "pieceGranularity", default = 0, min = 0, max = 100, 
-										 update = updatePieceGranularity)
-	
+    wallThickness = props.IntProperty(name = "wallThickness", default = 1, min = 1, max = 100,
+                                      update = updateWallThickness)
+    pieceGranularity = props.IntProperty(name = "pieceGranularity", default = 0, min = 0, max = 100, 
+                                         update = updatePieceGranularity)
+    
 def initialize():
-	#print("HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-#	utils.register_class(DestructionContext)
-	Object.destruction = props.PointerProperty(type = DestructionContext, name = "DestructionContext")  
+#   print("HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+#   utils.register_class(DestructionContext)
+    Object.destruction = props.PointerProperty(type = DestructionContext, name = "DestructionContext")
+    dd.DataStore.proc = Processor()  

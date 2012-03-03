536f73db97923304fc5a0a415d7e329d30b6eb01
Martin Felke <martin.felke@googlemail.com>
2012-03-03 19:49:05 +0100

    setup file update for win/macosx

--- object_destruction/libvoro/setup.py
+++ object_destruction/libvoro/setup.py
@@ -2,23 +2,36 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import platform
+import shutil
 
 extname = "voronoi"
+
 if platform.architecture()[0] == "64bit":
     if platform.architecture()[1] == "ELF":
         extname = "linux64/voronoi"
-    elif platform.architecture()[1] == "WindowsPE":
-        extname = "win64/voronoi"
-    else:
-        extname = "osx64/voronoi"
 elif platform.architecture()[0] == "32bit":
     if platform.architecture()[1] == "ELF":
         extname = "linux32/voronoi"
-    elif platform.architecture()[1] == "WindowsPE":
-        extname = "win32/voronoi"
-    else:
-        extname = "osx32/voronoi"
+
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension(extname, [ "voro/src/voro++.cc", "voronoi.pyx"], language="c++")]
 )
+
+if platform.architecture()[0] == "64bit":
+    if platform.architecture()[1] == "WindowsPE":
+        dest = "win64/"+extname+".pyd"
+        src = extname+".pyd"
+    else:
+        dest = "osx64/"+extname+".so"
+        src = extname+".so"
+elif platform.architecture()[0] == "32bit":
+    if platform.architecture()[1] == "WindowsPE":
+        dest = "win32/" + extname + ".pyd"
+        src = extname + ".pyd"
+    else:
+        dest = "osx32/"+extname+".so"
+        src = extname+".so"
+
+if platform.architecture()[1] != 'ELF':
+    shutil.copyfile(src, dest)

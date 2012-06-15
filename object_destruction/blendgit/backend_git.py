e1f7171d74ec405b07d410eac8a02ee62a4571fa
Martin Felke <martin.felke@googlemail.com>
2012-06-15 20:56:57 +0200

    basic commit and update, has still flaws

--- object_destruction/blendgit/backend_git.py
+++ object_destruction/blendgit/backend_git.py
@@ -15,19 +15,114 @@ class Git():
         self.git = '/usr/bin/git'
         self.work = work
         os.chdir(self.work)
-          
-    def init(self, repo):
-        self.command("init", ["--separate-git-dir=" + repo, self.work])
         
+    # Getting and Creating Projects 
+    #def init(self, quiet=False, bare=False, template ="", separate = "", shared = "")
+    def init(self):
+        self.command("init", [self.work])
+        
+#    def clone(self, template = "", local = False, shared = False, no_hardlinks = False, 
+#              quiet = False, n = False, bare = False, mirror = False, origin = "", 
+#              branch = "", upload_pack = "", reference = "", separate_git_dir = "", 
+#              depth = 0, recursive = False, repository)       
+#        pass
+
+    # Basic Snapshotting
+        
+#    def add(self, n = False, v = False, force = False, interactive = False, patch = False,
+#            edit = False, all = False, update = False, intent_to_add = False, refresh = False,
+#            ignore_errors = False, ignore_missing = False, filepattern)
     def add(self, file):
-        #filename = os.path.basename(file)
         self.command("add", [file])
+        
+#    def status(self, short = False, branch = False, porcelain = False, untracked_files = "", 
+#              ignore_submodules = "", ignored = False, z = False, pathspec) 
+#        #self.command("status", [])
+#        pass
     
-    def commit(self, file, message):
+    def status(self, file):
+        return self.command("status", [file])
+    
+    #def diff(self):
+    #    pass    
+    
+#    def commit(self, interactive = False, all = False, patch = False, s = False, v = False,
+#               u = "", amend = False, dry_run = False, c = "", C = "", fixup = "",
+#               squash = "", F = "",  message = "", reset_author = False, allow_empty = False,
+#               allow_empty_message = False, no_verify = False, e = False, author = ""
+#               date = "", cleanup = "", status = False, no_status = False, i = False, o = False)
+    def commit(self, file, message):           
         self.command("commit", ['-m', message, file])
     
-    def status(self):
-        self.command("status", [])
+    def reset(self, file):
+        self.command("reset", ['--hard', file])
+    
+    def rm(self, file):
+        self.command("rm", [file])
+    
+#    def mv(self):
+#        pass
+    
+
+
+    #Branching and Merging 
+#    def branch(self):
+#        pass   
+#    
+#    def checkout(self):
+#        pass
+    
+    def log(self, file):
+        return self.command("log", [file]) 
+    
+    def update(self, file, commit):
+        outRaw = self.command("ls-tree", [commit])
+        out = outRaw.stdout.read().decode("utf-8")
+        blobnr = self.blobnr(out, file)
+        if blobnr != None:
+            blob = self.command("cat-file", ["blob", blobnr])
+            tmp = open("tmp.blend", "wb+")
+            tmp.write(blob.stdout.read())
+            tmp.close()
+        
+    
+    def blobnr(self, commitdata, file):
+        lines = commitdata.split("\n")
+        print("L", lines)
+        for l in lines:
+            tab = l.split("\t")
+            name = tab[1]
+            blob = tab[0].split(" ")[1]
+            blobnr = tab[0].split(" ")[2]
+            #print("R", records)
+            
+            if "blob" == blob and file == name:
+                return blobnr
+        return None
+                    
+        
+    
+#    def merge(self):
+#        pass
+#
+#    def tag(self):
+#        pass
+
+    # Sharing and Updating Projects
+#    
+#    def fetch(self):
+#        pass
+#    
+#    def push(self):
+#        pass
+#    
+#    def pull(self):
+#        pass
+#   
+#    def remote(self):
+#        pass
+        
+    # Miscellaneous
     
     def ignore(self, file, asPattern):
         os.chdir(self.work)
@@ -37,55 +132,25 @@ class Git():
         else:
             ig.write("!" + file + "\n")
         ig.close()
-        
-#    def checkout(self, path):
-#        pass
-#    
-#    def clone(self, target):
-#        pass
-#    
-#    def log(self):
-#        pass 
-#    
-#    def pull(self):
-#        pass
-#    
-#    def push(self):
-#        pass
-#    
-#    def rebase(self):
-#        pass
-#    
-#    def reset(self):
-#        pass
-#    
-#    def rm(self):
-#        pass
-#    
-#    def show(self):
-#        pass
-#    
-#    def tag(self):
-#        pass
 #        
-    #generic Git command with textual feedback        
+#    def show(self)
+#        pass
+#    
+#    def rebase(self)
+#        pass
+#    
+#    def grep(self)
+#        pass
+#    
+#    def bisect(self)
+#        pass
+    
+    #generic Git command with textual feedback  
+          
     def command(self, cmd, args): 
         p = subprocess.Popen([self.git, cmd] + args,
             stdout = subprocess.PIPE, 
             stderr = subprocess.STDOUT)
-        out = p.stdout.read().decode("utf-8") # convert bytes to string
-        print(out)
-
-#def test():
-#    repo = '/home/xxx/TestRepo/'
-#    file = '/home/xxx/Dokumente/blender/TEST/GitWork/macroTest.blend'  
-#    work = '/home/xxx/Dokumente/blender/TEST/GitWork/'     
-#    g = Git(repo, work)
-#   # g.init()
-#   # g.add(file)
-#  #  g.ignore("*.blend1", True)
-##    g.ignore("*.blend2", True)
-#  #  g.commit(file, "MacroTest Initial Commit")
-#    g.status()
-#
-##test() 
+        #out = p.stdout.read().decode("utf-8") # convert bytes to string
+        #print(out)
+        return p
--- object_destruction/blendgit/frontend_git.py
+++ object_destruction/blendgit/frontend_git.py
@@ -8,26 +8,70 @@ class GitInit(bpy.types.Operator):
     bl_label = "Init"
     
     def execute(self, context):
-        g = b.Git(context.scene.workdir)
-        g.init(context.scene.repo)
+        g = b.Git(context.scene.git.workdir)
+        g.init()
         return {'FINISHED'}
 
-class GitStatus(bpy.types.Operator):
-    bl_idname = "git.status"
-    bl_label = "Status"
-   
+class GitLog(bpy.types.Operator):
+    bl_idname = "git.log"
+    bl_label = "Log"
+    
+    
+    def populateLog(self, context, log):
+        records = log.split("\n\n")
+        commit = None
+        author = None
+        date = None
+        
+        #print("R:", records)
+        for i in range(0, len(records)):
+               
+            if i % 2 == 0:
+                lines = records[i].split("\n")
+                #print("L:", lines)
+                commit = lines[0].split(" ")[1]
+                author = lines[1].split(": ")[1]
+                date = lines[2].split(": ")[1]
+            else:
+                message = records[i].split("\n")[0] 
+                    
+                entry = context.scene.git.history.add()
+                entry.message = message
+                entry.commit = commit
+                entry.author = author
+                entry.date = date
+                
+                #the display name ?
+                entry.name = message + " " + date + " " + commit
+    
+    def isRepo(self, context, g):
+        s = g.status(context.scene.git.file)
+        stat = s.stdout.read().decode("utf-8")
+        if "fatal: Not a git repository" in stat: #very hackish
+            return False
+        return True    
+    
     def execute(self, context):
-        g = b.Git(context.scene.workdir)
-        g.status()
+        g = b.Git(context.scene.git.workdir)
+        
+        if self.isRepo(context, g):
+            logRaw = g.log(context.scene.git.file)
+            log = logRaw.stdout.read().decode("utf-8")
+            context.scene.git.history.clear()
+            
+            #print("LOG", log)
+            if log != "":
+                self.populateLog(context, log)
+        
         return {'FINISHED'}
     
-class GitAdd(bpy.types.Operator):
-    bl_idname = "git.add"
-    bl_label = "Add"
+class GitReset(bpy.types.Operator):
+    bl_idname = "git.reset"
+    bl_label = "Reset"
     
     def execute(self, context):
-        g = b.Git(context.scene.workdir)
-        g.add(context.scene.file)
+        g = b.Git(context.scene.git.workdir)
+        g.reset(context.scene.git.file)
         return {'FINISHED'}
 
 class GitCommit(bpy.types.Operator):
@@ -35,9 +79,53 @@ class GitCommit(bpy.types.Operator):
     bl_label = "Commit"
     
     def execute(self, context):
-        g = b.Git(context.scene.workdir)
-        g.commit(context.scene.filename, context.scene.message)
+        g = b.Git(context.scene.git.workdir) 
+        s = g.status(context.scene.git.file)
+        status = s.stdout.read().decode("utf-8")
+        print(s.stdout.read())
+        
+        if "fatal: Not a git repository" in status: #very hackish
+            print("Init and Add")
+            g.init()
+            g.add(context.scene.git.file)
+        elif "Untracked" in status:
+            print("Add")
+            g.add(context.scene.git.file)
+        
+        g.commit(context.scene.git.file, context.scene.git.msg)
+        bpy.ops.git.log()
         return {'FINISHED'}
+    
+class GitUpdate(bpy.types.Operator):
+    bl_idname = "git.update"
+    bl_label = "Update"
+    
+    def execute(self, context):
+        g = b.Git(context.scene.git.workdir) 
+        index = context.scene.git.active_entry
+        entry = context.scene.git.history[index]
+        g.update(context.scene.git.file, entry.commit)
+        #load file here
+        bpy.ops.wm.open_mainfile(filepath="tmp.blend")
+         
+        return {'FINISHED'}
+
+class LogEntry(bpy.types.PropertyGroup):
+    
+    message = bpy.props.StringProperty(name = "message")
+    author = bpy.props.StringProperty(name = "author")
+    date = bpy.props.StringProperty(name = "date")
+    commit = bpy.props.StringProperty(name = "commit")
+    
+
+class GitContext(bpy.types.PropertyGroup):
+    
+    workdir = bpy.props.StringProperty(name = "workdir")
+    file = bpy.props.StringProperty(name = "file")
+    msg = bpy.props.StringProperty(name = "msg")
+    active_entry = bpy.props.IntProperty(name = "active_entry")
+    history = bpy.props.CollectionProperty(type = LogEntry, name = "history")
+    
 
 class GitPanel(bpy.types.Panel):
     bl_idname = "OBJECT_PT_git"
@@ -48,54 +136,58 @@ class GitPanel(bpy.types.Panel):
     
     def register():
         
-        bpy.types.Scene.workdir = bpy.props.StringProperty(name = "workdir")
-        bpy.types.Scene.repo = bpy.props.StringProperty(name = "repo") 
-        bpy.types.Scene.file = bpy.props.StringProperty(name = "file")
-        bpy.types.Scene.msg = bpy.props.StringProperty(name = "msg")
+        bpy.types.Scene.git = bpy.props.PointerProperty(type = GitContext, name = "GitContext")
         bpy.app.handlers.load_post.append(GitPanel.file_handler)
         bpy.app.handlers.save_post.append(GitPanel.file_handler)
     
     def unregister():
-        del bpy.types.Scene.workdir
-        del bpy.types.Scene.repo
-        del bpy.types.Scene.file
-        del bpy.types.Scene.msg
+        del bpy.types.Scene.git
      
     def draw(self, context):
         
         layout = self.layout
         
-        row = layout.row(align=True)
-        row.prop(context.scene, "workdir", text = "Working Directory")
-        props = row.operator("buttons.directory_browse", text = "", icon = 'FILE_FOLDER')
-        props["filepath"] = context.scene.workdir
-        layout.operator("git.status")
+#        row = layout.row(align=True)
+#        row.prop(context.scene, "workdir", text = "Working Directory")
+#        props = row.operator("buttons.directory_browse", text = "", icon = 'FILE_FOLDER')
+#        props["filepath"] = context.scene.workdir
+#        layout.operator("git.status")
+#        
+#        row = layout.row(align=True)
+#        row.prop(context.scene, "repo", text = "Repository Path")
+#        props = row.operator("buttons.directory_browse", text = "", icon = 'FILE_FOLDER')
+#        props["filepath"] = context.scene.repo
+#        layout.operator("git.init")
+#        
+#        row = layout.row(align=True)
+#        row.prop(context.scene, "file", text = "File to add")
+#        props = row.operator("buttons.file_browse", text = "", icon = 'FILESEL')
+#        props["filepath"] = context.scene.file
+#        layout.operator("git.add")
         
-        row = layout.row(align=True)
-        row.prop(context.scene, "repo", text = "Repository Path")
-        props = row.operator("buttons.directory_browse", text = "", icon = 'FILE_FOLDER')
-        props["filepath"] = context.scene.repo
-        layout.operator("git.init")
+#        row = layout.row(align=True)
+#        row.prop(context.scene, "file", text = "File to commit")
+#        props = row.operator("buttons.file_browse", text = "", icon = 'FILESEL')
+#        props["filepath"] = context.scene.file
         
-        row = layout.row(align=True)
-        row.prop(context.scene, "file", text = "File to add")
-        props = row.operator("buttons.file_browse", text = "", icon = 'FILESEL')
-        props["filepath"] = context.scene.file
-        layout.operator("git.add")
+        layout.label("History")
+        layout.template_list(context.scene.git, "history", context.scene.git, "active_entry" , rows = 5)
         
-        row = layout.row(align=True)
-        row.prop(context.scene, "file", text = "File to commit")
-        props = row.operator("buttons.file_browse", text = "", icon = 'FILESEL')
-        props["filepath"] = context.scene.file
-        layout.prop(context.scene, "msg", text = "Commit Message")
-        layout.operator("git.commit")
+        if context.scene.git.file != "": 
+            layout.operator("git.update")
+            
+            layout.prop(context.scene.git, "msg", text = "Message")        
+            layout.operator("git.commit")
+            
+            #layout.operator("git.reset") need to unload file first ?
         
     @persistent
     def file_handler(dummy):
-        print("Load Handler:", bpy.data.filepath)
+        print("File Handler:", bpy.data.filepath)
         currentfile = bpy.path.basename(bpy.data.filepath)
         currentdir = bpy.path.abspath("//")
         
-        bpy.context.scene.workdir = currentdir        
-        bpy.context.scene.repo = currentdir
-        bpy.context.scene.file = currentfile
+        bpy.context.scene.git.workdir = currentdir        
+        bpy.context.scene.git.file = currentfile
+        if currentdir != "" and currentfile != "":
+            bpy.ops.git.log()
